dc59e2e42060a3c843c564b6c7322ed8b8d6e5df
seman <seman@DESKTOP-VCCSGRP>
2023-07-17 17:24:34 +0300

    Add project files.

---
+++ MusteriManagers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+      class MusteriManagers
+    {
+       public  void MusteriGoruntule(Musteri musteri )
+        {
+
+            Console.WriteLine("Müşterinin adı :  " + musteri.Ad + "\n" +"Müşterinin Soyadı : "+ musteri.Soyad + "\n"+ "Müşterinin Maaşı : " +musteri.maas );
+
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " kaydı başarıyla silindi.");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+
+            musteri1.ID = 13;
+            musteri1.Ad = "Almuni";
+            musteri1.Soyad = "Meow";
+            musteri1.maas = 35000;
+
+
+            Musteri musteri2 = new Musteri();
+
+            musteri2.ID = 14;
+            musteri2.Ad = "Semanur";
+            musteri2.Soyad = "Meow";
+            musteri2.maas = 50000;
+
+            MusteriManagers musteriManagers = new MusteriManagers();
+
+            musteriManagers.MusteriGoruntule(musteri1);
+            musteriManagers.Sil(musteri1);
+            musteriManagers.MusteriGoruntule(musteri2);
+            musteriManagers.Sil(musteri2);
+
+
+        }
+    }
+}
